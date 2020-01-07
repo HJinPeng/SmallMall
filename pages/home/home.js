9cb5e5a0dfa44b23fd7eff35e1b5bfe900534323
HJinPeng <742463015@qq.com>
2020-01-07 20:28:40 +0800

    注册页面的配置

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,11 @@
 // pages/home/home.js
+const app = getApp();
+const name = app.globalData.name;
+const age = app.globalData.age;
+// console.log(name);
+// console.log(age);
+
+// 注册一个页面
 Page({
 
   /**
@@ -7,27 +14,30 @@ Page({
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // onLoad: function (options) {
+  // },
+  onLoad(){
+    // console.log('onload');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // console.log('onShow');
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    // console.log('onReady');
+  },
+
+
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -62,5 +72,26 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 监听wxml中的事件
+  handleGetUserInfo(event){
+    // console.log(event);
+  },
+  clickItem(){
+    // console.log("haha");
+  },
+
+  // 监听其他事件
+  // 监听页面滚动
+  onPageScroll(obj){
+    // console.log(obj);
+  },
+
+  // 监听页面滚动到底部
+  onReachBottom(){
+    // console.log();
   }
+
+
 })
