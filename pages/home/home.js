c9222b8640ec1dd362b27126bb0d2832b3c32454
HJinPeng <742463015@qq.com>
2020-01-09 21:49:43 +0800

    网络请求和封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
 // pages/home/home.js
+import request from '../../util/request.js';
 Page({
+  onLoad(){
+    this.getData();
+    // wx.request({
+    //   url: 'https://www.baidu.com/',
+    //   method:'post',
+    //   data:{},
+    //   success: res => {
+    //     console.log(res);
+    //   },
+    //   fail: err => {
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    //   },
+    //   // 成功失败都会执行
+    //   complete: res => {
 
+    //   }
+    // })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getData(){
+    request({
+      url:'https://www.baidu.com/'
+    }).then(res=>{
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    })
   }
 })
